d9db9c3dded298f1084f453219626f52e22c1074
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-17 19:39:18 +0100

    username and email are unique upon creation now

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   name: { type: String, default: "" },
   bio: { type: String, default: "" },
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
